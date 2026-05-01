0fce52d974e3ddf7cf43536dd2d5d43e25bb2bb2
Maksim Popovski <281952@stud.inn.no>
2026-05-01 21:54:24 +0200

    Added biomes
    I added the biomes and their functionality. There’s still more work to do, but overall everything is going in the right direction so far. The only thing left is to add more level blocks with traps

--- Source/Dragons_lair_project/LevelGeneratorv2.h
+++ Source/Dragons_lair_project/LevelGeneratorv2.h
@@ -4,6 +4,12 @@
 #include "GameFramework/Actor.h"
 #include "LevelGeneratorv2.generated.h"
 
+UENUM(BlueprintType)
+enum class EBiomeType : uint8
+{
+    Biome1, Biome2, Biome3
+};
+
 UCLASS()
 class DRAGONS_LAIR_PROJECT_API ALevelGeneratorv2 : public AActor
 {
@@ -16,46 +22,53 @@ protected:
     virtual void BeginPlay() override;
     virtual void Tick(float DeltaTime) override;
 
-    /** Movement Actor that drives the world towards the player */
     UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "Runner | Setup")
     AActor* MovementParent;
 
-    /** * Block Blueprints:
-     * Index 0: STARTER BLOCK (Spawns only once at the start)
-     * Index 1 to N: VARIATION BLOCKS (Spawned randomly in a shuffled sequence)*/
-    UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "Runner | Setup")
+    UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "Runner | Level Blocks")
     TArray<TSubclassOf<AActor>> BlockTemplates;
 
-    /** Fixed length of a single block in Unreal Units (1500 +-= 15 meters) */
-    UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "Runner | Settings")
-    float BlockLength = 1500.0f;
+    UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "Runner | Biomes")
+    TArray<TSubclassOf<AActor>> Biome1Pool;
 
-    /** Number of blocks maintained ahead of the player */
-    UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "Runner | Settings")
-    int32 MaxBlocksAhead = 8;
+    UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "Runner | Biomes")
+    TArray<TSubclassOf<AActor>> Biome2Pool;
 
-    /** X Coordinate behind the player where blocks get destroyed */
+    UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "Runner | Biomes")
+    TArray<TSubclassOf<AActor>> Biome3Pool;
+
+    UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "Runner | Progress")
+    float ProgressIncrement = 0.00025f;
+
+    UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "Runner | Progress")
+    float Biome3Threshold = 0.5f;
+
+    UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "Runner | Settings")
+    float LevelBlockLength = 3600.0f;
+
+    UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "Runner | Settings")
+    int32 MaxBlocksAhead = 4;
+
     UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "Runner | Settings")
     float DeletionXThreshold = -3000.0f;
 
 private:
-    /** List of indices for Variation Blocks to ensure unique random order */
     TArray<int32> ShuffleBag;
 
-    /** Current list of spawned blocks */
     UPROPERTY()
     TArray<AActor*> ActiveBlocks;
 
-    /** Reference to the last spawned block to calculate next spawn position */
     UPROPERTY()
     AActor* LastSpawnedBlock;
 
-    /** Core function to spawn a block at a specific world location */
+    EBiomeType CurrentBiome;
+    int32 FloorsInCurrentChunk;
+    int32 MaxFloorsInChunk;
+    float InternalProgress;
+
     void SpawnBlock(TSubclassOf<AActor> BlockClass, FVector Location);
-
-    /** Returns a random index from BlockTemplates (excluding index 0) with no repeats until full cycle is done */
     int32 GetNextBlockIndex();
-
-    /** Removes blocks that have moved past the player */
+    TSubclassOf<AActor> GetNextBiomeClass();
+    void ReplaceFloorsWithBiomes(AActor* ParentBlock);
     void CleanupBlocks();
 };

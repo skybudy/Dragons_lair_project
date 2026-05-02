a571072739f9879ccaca70c386e3eac889e135b7
skybudy <286853@stud.inn.no>
2026-05-02 16:15:45 +0300

    Readded Enemy c++

---
+++ Source/Dragons_lair_project/Private/Enemy.cpp
@@ -0,0 +1,57 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+
+#include "Enemy.h"
+
+#include "Components/CapsuleComponent.h"
+#include "Components/SphereComponent.h"
+
+
+// Sets default values
+AEnemy::AEnemy()
+{
+	// Set this character to call Tick() every frame.  You can turn this off to improve performance if you don't need it.
+	PrimaryActorTick.bCanEverTick = false;
+	SkeletalMesh = CreateDefaultSubobject<USkeletalMeshComponent>(TEXT("SkeletalMesh"));
+	EnemyAttackRange = CreateDefaultSubobject<UCapsuleComponent>(TEXT("EnemyAttackRange"));
+	EnemyVisionRange = CreateDefaultSubobject<USphereComponent>(TEXT("EnemyVisionRange"));
+	// StateTreeComponent = CreateDefaultSubobject<UStateTreeComponent>(TEXT("StatTree"));
+	
+	RootComponent = SkeletalMesh;
+	EnemyAttackRange ->SetupAttachment(SkeletalMesh);
+	EnemyVisionRange ->SetupAttachment(SkeletalMesh);
+
+}
+
+// Called when the game starts or when spawned
+void AEnemy::BeginPlay()
+{
+	Super::BeginPlay();
+	FGameTime = .4;
+	bSeesPlayer = false;
+	bPlayerInAttackRange = false;
+	bQTEComplete = false;
+	
+}
+
+void AEnemy::OnVisionSphereBeginOverlap(UPrimitiveComponent* OverlappedComp, AActor* OtherActor,
+	UPrimitiveComponent* OtherComp, int32 OtherBodyIndex, bool bFromSweep, const FHitResult& SweepResult)
+{
+	//This is the cast function
+	ACharacter* OverlappingCharacter = Cast<ACharacter>(OtherActor);
+	if (OverlappingCharacter)
+	{
+		bSeesPlayer = true;
+	}
+}
+
+void AEnemy::OnAttackRangeBeginOverlap(UPrimitiveComponent* OverlappedComp, AActor* OtherActor,
+	UPrimitiveComponent* OtherComp, int32 OtherBodyIndex, bool bFromSweep, const FHitResult& SweepResult)
+{
+	//This is the cast function
+	ACharacter* OverlappingCharacter = Cast<ACharacter>(OtherActor);
+	if (OverlappingCharacter)
+	{
+		bPlayerInAttackRange = true;
+	}
+}
